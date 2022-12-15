99d2f0097bea2c7483268ec5b36a7c69be2fc9ab
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-15 16:23:49 +0530

    Adding js file.

--- js/random.js
+++ js/random.js
@@ -1,8 +1,15 @@
-function random()
+btn=document.createElement("Button");
+btn.innerHTML="click";
+document.body.appendChild(btn);
+
+flex=document.createElement("p");
+document.body.appendChild(flex);
+	
+btn.onclick=function()
 {
 	var random_number=(Math.random()*6)
 	random_number = Math.floor(random_number);
-	return random_number;
+	flex.innerHTML=random_number;
 }
 random()
 
